953363cab257b44ec9cfb9a3e8b994fce4d3b3e4
Ted Fan <ted.fan@ciandt.com>
2023-11-23 16:22:02 +1100

    Add handler and handler test

---
+++ handler.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { sendNotifications } = require("./Infobip");
+const app = express();
+const port = 7073;
+
+app.use(express.json());
+
+const EVENT_TYPE = Object.freeze({
+  ORDER_CONFIRMATION: "ORDER_CONFIRMATION",
+});
+
+app.post("/webapi/notification", async (req, res) => {
+  const { event, notifications } = req.body;
+  const { customerId, orderId } = notifications[0]; // TODO: batch processing
+
+  if (event === EVENT_TYPE.ORDER_CONFIRMATION) {
+    console.log("Order confirmation received:", req.body);
+    const response = await sendNotifications(customerId, orderId);
+    res.status(200).send(response);
+  } else {
+    res.status(400).send("Invalid event type");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+module.exports = app;
---
+++ handler.test.js
@@ -0,0 +1,59 @@
+const request = require("supertest");
+const app = require("./handler");
+jest.mock("./Infobip");
+
+const { sendNotifications } = require("./Infobip");
+
+describe("/webapi/notification", () => {
+  beforeEach(() => {
+    sendNotifications.mockClear();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("should respond with 200 for ORDER_CONFIRMATION event", async () => {
+    const mockEvent = {
+      event: "ORDER_CONFIRMATION",
+      notifications: [
+        {
+          customerId: "12345",
+          orderId: "1010101010",
+        },
+      ],
+    };
+    const mockAPIResponse = {
+      operationId: "8F78025B-AB9A-475B-A4A3-078BBC4FC237",
+    };
+
+    sendNotifications.mockResolvedValue(mockAPIResponse);
+
+    const response = await request(app)
+      .post("/webapi/notification")
+      .send(mockEvent);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe(JSON.stringify(mockAPIResponse));
+    expect(sendNotifications).toHaveBeenCalledWith("12345", "1010101010");
+  });
+
+  test("should respond with 400 for invalid event type", async () => {
+    const mockEvent = {
+      event: "INVALID_EVENT",
+      notifications: [
+        {
+          customerId: "67890",
+          orderId: "vwxyz",
+        },
+      ],
+    };
+
+    const response = await request(app)
+      .post("/webapi/notification")
+      .send(mockEvent);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toBe("Invalid event type");
+  });
+});
